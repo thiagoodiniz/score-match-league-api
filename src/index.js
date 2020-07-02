f5a7323ca8e210964fbece623e258b0940e4401f
Thiago Diniz <thiago.diniz@apllos.com>
2020-07-02 16:53:53 -0300

    enable cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();         
 const bodyParser = require('body-parser');
 const swaggerDoc = require('./swagerDoc.js');
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 require('./controllers/leagueController')(app);
 require('./controllers/playerController')(app);
 swaggerDoc(app);
 
-const port = 3000;
+const port = 3001;
 
 app.listen(port);
 console.log(`API escutando na porta ${port}`);
